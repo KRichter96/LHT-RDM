1df04c96f23e40d2a5a301b068fa1ab4f158daf2
fortis-it-dev <53822188+fortis-it-dev@users.noreply.github.com>
2019-11-07 09:57:06 +0100

    updates on part-details and getting id

--- src/app/pages/part-detail/part-detail.page.ts
+++ src/app/pages/part-detail/part-detail.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ServerRepositoryService } from 'src/app/services/server/serverrepository.service';
+import { PartModel } from 'src/app/models/part/partmodel';
+import { PartsPage } from '../parts/parts.page';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-part-detail',
@@ -6,12 +11,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./part-detail.page.scss'],
 })
 export class PartDetailPage implements OnInit {
+  partItem: PartModel;
+  id: string;
 
   identificationNumber =  null;
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute, private repoService: ServerRepositoryService) { }
 
   ngOnInit() {
-  }
+  this.id = this.route.snapshot.paramMap.get('id');
 
+//  this.repoService.getData().then(partModelObj => {
+//    this.partItem = partModelObj.parts[id];
+//  });
+  }
 }
