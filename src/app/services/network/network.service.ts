df69f2b356bbf027ea60686fa4306ce54a137eb1
Felix Gaschler <felix.gaschler@fortis-it.de>
2020-02-12 20:07:19 +0100

    network: add timeout to health check calls

--- src/app/services/network/network.service.ts
+++ src/app/services/network/network.service.ts
@@ -1,10 +1,11 @@
 import {Network} from '@ionic-native/network/ngx';
 import {Injectable} from '@angular/core';
-import {BehaviorSubject, Observable} from 'rxjs';
+import {BehaviorSubject, Observable, of} from 'rxjs';
 import {Platform} from '@ionic/angular';
 import {HttpClient} from '@angular/common/http';
 import {ToastService} from '../toast/toast.service';
 import {BackendUrlProviderService} from '../backend-url-provider/backend-url-provider.service';
+import {catchError, map, timeout} from 'rxjs/operators';
 
 export enum ConnectionStatus { Online, Offline }
 
@@ -27,9 +28,21 @@ export class NetworkService {
   }
 
   checkConnection() {
-    this.http.get(this.projectUrl).subscribe(
-        data => { this.updateNetworkStatus(ConnectionStatus.Online, data); },
-        error => { this.updateNetworkStatus(ConnectionStatus.Offline, error); }
+    this.http.get(this.projectUrl).pipe(
+      timeout(2000),
+        catchError(error => {
+          this.updateNetworkStatus(ConnectionStatus.Offline, error).then();
+          return of(null);
+        }
+      )).subscribe(
+      data => {
+        if (data === null) {
+          this.updateNetworkStatus(ConnectionStatus.Offline, data).then();
+        } else {
+          this.updateNetworkStatus(ConnectionStatus.Online, data).then();
+        }
+      },
+      error => { this.updateNetworkStatus(ConnectionStatus.Offline, error).then(); }
     );
   }
 
